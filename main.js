16dc7c0617440e57acda7c46862011d56093c36a
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-06-15 11:24:24 -0700

    Add JS to hide main page when "Create Event Button" clicked. #2

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
+
+/* update form data array */
 var data = []
 
 window.addEventListener('DOMContentLoaded', function (event) {
-  var $eventForm = document.querySelector('#event-form')
+  var $eventForm = document.querySelector('#event')
 
   $eventForm.addEventListener('submit', function (event) {
     event.preventDefault()
@@ -17,4 +19,16 @@ window.addEventListener('DOMContentLoaded', function (event) {
     }
     data.push(newData)
   })
+})
+
+/* navigate from main page to event form */
+var $createButton = document.querySelector('#new-button')
+
+$createButton.addEventListener('click', function (event) {
+  event.preventDefault()
+  var $mainPage = document.querySelector('.main-page')
+  var $eventForm = document.querySelector('#event')
+
+  $mainPage.setAttribute('class', 'hidden main-page')
+  $eventForm.setAttribute('class', 'container-fluid')
 })
