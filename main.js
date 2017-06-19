1e05127be793321b22e08a32f9c118ee11972148
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-06-19 16:01:56 -0700

    Current state. #3

--- main.js
+++ main.js
@@ -3,10 +3,34 @@
 var data = []
 
 function formMain() {
-  var $mainPage = document.querySelector('.main')
-  var $eventForm = document.querySelector('#event')
-  $mainPage.setAttribute('class', 'main')
-  $eventForm.setAttribute('class', 'hidden container-fluid')
+  var $mainPage = document.querySelector('.hidden.view-main')
+  var $eventForm = document.querySelector('.view-form')
+  $mainPage.setAttribute('class', 'view-main')
+  $eventForm.setAttribute('class', 'hidden view-form')
+}
+
+function mainForm() {
+  var $mainPage = document.querySelector('.view-main')
+  var $eventForm = document.querySelector('.hidden.view-form')
+  $mainPage.setAttribute('class', 'hidden view-main')
+  $eventForm.setAttribute('class', 'view-form')
+}
+
+function makeEvent(events) {
+  var $start = document.querySelector('.start')
+  var $tempLi = document.createElement('li')
+  var $tempP = document.createElement('p')
+
+  $start.insertAdjacentElement('afterend', $tempLi)
+  $tempLi.insertAdjacentElement('beforeend', $tempP)
+
+  $tempLi.textContent = events.title
+  $tempP.textContent = 'content'
+
+  // $tempLi.textContent = events.title
+  // console.log($tempLi.textContent)
+  // $tempP.textContent = events.date
+  // console.log($tempP.textContent)
 }
 
 window.addEventListener('DOMContentLoaded', function (event) {
@@ -27,14 +51,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
     data.push(newData)
 
     for (var i = 0; i < data.length; i++) {
-      var $newTitle = document.querySelector('.start')
-      var $tempLi = document.createElement('li')
-      $newTitle.insertAdjacentElement('afterend', $tempLi)
-      $tempLi.textContent = data[i].title
-      var $tempP = document.createElement('p')
-      $tempLi.insertAdjacentElement('beforeend', $tempP)
-      $tempP.textContent = data[i].date
-      $tempLi.classList.add('event')
+      makeEvent(data[i])
     }
 
     formMain()
@@ -43,24 +60,11 @@ window.addEventListener('DOMContentLoaded', function (event) {
 
 var $createButton = document.querySelector('#new-button')
 
-$createButton.addEventListener('click', function (event) {
-  event.preventDefault()
-  var $mainPage = document.querySelector('.main')
-  var $eventForm = document.querySelector('#event')
-
-  $mainPage.setAttribute('class', 'hidden main')
-  $eventForm.setAttribute('class', 'container-fluid')
-})
+$createButton.addEventListener('click', mainForm)
 
 var $toMain = document.querySelector('#form-return')
 
-$toMain.addEventListener('click', function (event) {
-  event.preventDefault()
-  var $mainPage = document.querySelector('.main')
-  var $eventForm = document.querySelector('#event')
-
-  formMain()
-})
+$toMain.addEventListener('click', formMain)
 
 var $current = document.querySelector('.end')
 $current.textContent = 'Current Date: ' + Date()
